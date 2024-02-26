971109be25ce1b267295d7a5a22ad31eb2c11d65
Tomshann <116895142+Tomshann@users.noreply.github.com>
2024-02-26 15:57:13 +0000

    Update models.py

---
+++ sustainability/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Team(models.Model):
+    COLORS = {
+        'FF0000': 'Red',
+        '0000FF': 'Blue',
+        '00FF00': 'Green'
+    }
+    team_name = models.CharField(max_length=100, primary_key=True)
+    team_color = models.CharField(max_length=6, choices=COLORS)
+
+
+class Stronghold(models.Model):
+    building_name = models.CharField(max_length=100, unique=True)
+    controlling_team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Coordinate(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    stronghold = models.ForeignKey(Stronghold, on_delete=models.CASCADE)
+
+
+class Action(models.Model):
+    action_name = models.CharField(max_length=100)
+    points_value = models.IntegerField()
+
+
+class Score(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    action_site = models.ForeignKey(Stronghold, on_delete=models.CASCADE)
+    action_done = models.ForeignKey(Action, on_delete=models.CASCADE)
+    datetime_earned = models.DateTimeField()
+
+class Player(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    pts_multiplier = models.FloatField(default=1.0)
