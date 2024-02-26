d7496cf5105fda11499bd862a47a802c32f2a1ef
Tomshann <116895142+Tomshann@users.noreply.github.com>
2024-02-26 15:48:22 +0000

    Add files via upload

---
+++ sustainability/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Team, Stronghold, Coordinate, Action, User, Score,Player
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+admin.site.register(Team)
+admin.site.register(Stronghold)
+admin.site.register(Coordinate)
+admin.site.register(Action)
+admin.site.register(Score)
+
+class PlayerInline(admin.StackedInline):
+    model = Player
+    can_delete = False
+    verbose_name_plural = 'player'
+
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = (PlayerInline,)
+
+# Unregister the default UserAdmin
+admin.site.unregister(User)
+
+# Register User with your custom admin
+admin.site.register(User, CustomUserAdmin)
