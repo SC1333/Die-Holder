becb5cc0abfa4b8f9c15f110c001892efefc86eb
tomsh <tomshannon2004@gmail.com>
2024-02-27 18:56:57 +0000

    Added comments

--- sustainability/urls.py
+++ sustainability/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('login/', Views.log_in, name='login'),
     path('register/', Views.register, name='register'),
     path('map/', Views.map, name='map'),
-    path('redeem-points/<int:buildingID>/<int:actionID>/', Views.redeem_points, name='redeem-points'),
-    path('get-building-and-action-names/<int:buildingID>/<int:actionID>/', Views.get_building_and_action_names, name='get-building-and-action-names'),#used as a getter for the database
-    path('write-to-score-table/', Views.write_to_score_table, name='write-to-score-table'),
+    path('redeem-points/<int:buildingID>/<int:actionID>/', Views.redeem_points, name='redeem-points'),#used as a getter for the database via a jquery request
+    path('get-building-and-action-names/<int:buildingID>/<int:actionID>/', Views.get_building_and_action_names, name='get-building-and-action-names'),#used as a getter for the database via a jquery request
+    path('write-to-score-table/', Views.write_to_score_table, name='write-to-score-table'),#used to write to the database score table via a jquery request
 ]
