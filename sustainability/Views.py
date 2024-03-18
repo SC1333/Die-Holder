24d6cc70f471a78c9c3b8fbd79bf6453116e081e
FedorMorgunov <fmorgunov2004@gmail.com>
2024-03-18 16:04:10 +0000

    fixed bugs

--- sustainability/Views.py
+++ sustainability/Views.py
@@ -330,7 +330,7 @@ def generate_totp_secret(request):
 
     # Convert image to data URI
     buffer = io.BytesIO()
-    img.save(buffer, format="PNG")
+    img.save(buffer)
     img_str = base64.b64encode(buffer.getvalue()).decode()
 
     data_uri = "data:image/png;base64," + img_str
